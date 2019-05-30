f444561e2363296074a061e8407ed151febaa5c8
Jean Carlo da Silva <jeaan3@gmail.com>
2019-05-29 22:56:53 -0300

    feat(details): melhorando tela de detalhes de filmes

--- lib/pages/movie_details_page.dart
+++ lib/pages/movie_details_page.dart
@@ -16,112 +16,142 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
   double movieRating = 2.5;
 
   var favColor = Colors.white;
+  bool fav = false;
 
   Movie get movie => widget.movie;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: _buildBody(movie),
+      backgroundColor: Colors.black,
     );
   }
 
   _buildBody(Movie movie) {
-    return _sliverList(movie);
+    return _sliver(movie);
   }
 
-  _sliverList(Movie movie) {
+  _sliver(Movie movie) {
     String name = movie.title;
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          iconTheme: IconThemeData(color: Colors.white, size: 30),
-          elevation: 8.0,
-          actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InkWell(
-                child: Icon(
-                  Icons.favorite,
-                  color: favColor,
-                ),
-                onTap: () {
-                  setState(() {
-                    favColor = Colors.red;
-                    //TODO salvar movie na base
-                    _onClickFav(context, movie);
-                  });
-                },
-              ),
-            )
-          ],
-          expandedHeight: 300.0,
-          pinned: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text(
-              name,
-              style: TextStyle(
-                  backgroundColor: Colors.black54, decorationThickness: 30),
-            ),
-            titlePadding: EdgeInsets.only(left: 50, bottom: 20),
-            collapseMode: CollapseMode.parallax,
-            background: _movieImage(movie),
-          ),
-        ),
-        SliverFillRemaining(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                  padding: EdgeInsets.all(16),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        movie.releaseDate,
-                        style: TextStyle(fontSize: 18),
-                      )
-                    ],
-                  )),
-              Container(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  movie.overview,
-                  style: TextStyle(fontSize: 20),
+
+    return SafeArea(
+        top: false,
+        bottom: false,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                iconTheme: IconThemeData(color: Colors.white, size: 40),
+                elevation: 8.0,
+                actions: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: InkWell(
+                      child: Icon(
+                        Icons.favorite,
+                        color: favColor,
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _onClickFav(context, movie);
+                          if (fav) {
+                            favColor = Colors.white;
+                          } else {
+                            favColor = Colors.red;
+                          }
+                        });
+                      },
+                    ),
+                  )
+                ],
+                expandedHeight: 300.0,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  titlePadding: EdgeInsets.only(left: 20, bottom: 20),
+                  collapseMode: CollapseMode.parallax,
+                  background: _movieImageBackdrop(movie),
                 ),
               )
-            ],
+            ];
+          },
+          body: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListView(
+              children: <Widget>[
+                Wrap(
+                  direction: Axis.horizontal,
+                  spacing: 50.0,
+                  alignment: WrapAlignment.spaceBetween,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Text(
+                        movie.title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(margin: EdgeInsets.only(top: 8.0, bottom: 8.0)),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Data de lançamento: ",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                    Text(
+                      movie.releaseDate,
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    )
+                  ],
+                ),
+                Container(margin: EdgeInsets.only(top: 8.0)),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      movie.overview,
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
-      ],
-    );
+        ));
   }
 
-  _movieImage(Movie movie) {
+  _movieImageBackdrop(Movie movie) {
     return Image.network(
       IMAGE_URL_500 + movie.pathBackdrop,
       fit: BoxFit.fitHeight,
     );
   }
 
-  _formattedDate(String date) {
-    DateTime dateFormatted = DateTime.parse(date);
+  _movieImagePoster(Movie movie) {
+    return Image.network(
+      IMAGE_URL_500 + movie.pathPoster,
+      fit: BoxFit.fitHeight,
+    );
   }
 
   Future _onClickFav(BuildContext context, movie) async {
-      final db = MovieDB.getInstance();
+    final db = MovieDB.getInstance();
 
-      final exists = await db.exists(movie);
+    final exists = await db.exists(movie);
 
-      if(exists) {
-        db.deleteMovie(movie.id);
-      } else {
-        int id = await db.saveMovie(movie);
-
-        print("Filme salvo! $id");
-      }
-
-
-
+    if (exists) {
+      db.deleteMovie(movie.id);
+      fav = false;
+    } else {
+      int id = await db.saveMovie(movie);
+      fav = true;
+      print("Filme salvo! $id");
+    }
   }
 }
-
